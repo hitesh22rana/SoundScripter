c54ff51d2cc836aab3c252a7a1816a9efa9a13ce
hitesh <hitesh22rana@gmail.com>
2023-09-17 17:36:50 +0530

    docs: file_manager utils docstring updated with typesafe params and return type

--- backend/app/utils/file_manager.py
+++ backend/app/utils/file_manager.py
@@ -87,10 +87,11 @@ class FileManager:
     @classmethod
     def make_directory(cls, directory: str) -> None:
         """
-        Make directory
-        :param directory: str
-        :return: None
+        Make directory if not already exists
+        :param -> directory: str
+        :return -> None
         """
+
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -98,36 +99,40 @@ class FileManager:
     def is_audio_file_extension(cls, file_extension: str) -> bool:
         """
         validate audio file extension
-        :param file_path: str
-        :return: bool
+        :param -> file_extension: str
+        :return -> bool
         """
+
         return file_extension in cls.audio_file_extensions
 
     @classmethod
     def is_video_file_extension(cls, file_extension: str) -> bool:
         """
         validate video file extension
-        :param file_path: str
-        :return: bool
+        :param -> file_extension: str
+        :return -> bool
         """
+
         return file_extension in cls.video_file_extensions
 
     @classmethod
     def is_transcription_file_extension(cls, file_extension: str) -> bool:
         """
         validate transcription file extension
-        :param file_path: str
-        :return: bool
+        :param -> file_extension: str
+        :return -> bool
         """
+
         return file_extension in cls.transcriptions_file_extensions
 
     @classmethod
     def is_valid_file_extension(cls, file_extension: str) -> bool:
         """
         validate payload file extension
-        :param file_path: str
-        :return: bool
+        :param -> file_extension: str
+        :return -> bool
         """
+
         return cls.is_audio_file_extension(
             file_extension=file_extension
         ) or cls.is_video_file_extension(file_extension=file_extension)
@@ -136,27 +141,30 @@ class FileManager:
     def is_audio_file(cls, file_type: str) -> bool:
         """
         validate audio file type
-        :param file_path: str
-        :return: bool
+        :param -> file_type: str
+        :return -> bool
         """
+
         return file_type in cls.audio_file_types
 
     @classmethod
     def is_video_file(cls, file_type: str) -> bool:
         """
         validate video file type
-        :param file_path: str
-        :return: bool
+        :param -> file_type: str
+        :return -> bool
         """
+
         return file_type in cls.video_file_types
 
     @classmethod
     def validate_payload_file_type(cls, file_type: str) -> bool:
         """
         Validate payload file type
-        :param file_type: str
-        :return: bool
+        :param -> file_type: str
+        :return -> bool
         """
+
         return cls.is_audio_file(file_type=file_type) or cls.is_video_file(
             file_type=file_type
         )
@@ -165,27 +173,29 @@ class FileManager:
     def validate_transcription_file_type(cls, file_type: str) -> bool:
         """
         Validate transcription file type
-        :param file_type: str
-        :return: bool
+        :param -> file_type: str
+        :return -> bool
         """
+
         return file_type in cls.transcription_file_types
 
     @classmethod
-    def get_unique_file_name(self) -> str:
+    def get_unique_file_id(self) -> str:
         """
         Generate a unique file name
-        :param file_name: str
-        :return: str
+        :return -> str
         """
+
         return datetime.now().strftime("%Y%m-%d%H-%M%S-") + str(uuid4())
 
     @classmethod
     def get_file_extension(cls, file_name: str) -> str:
         """
         Get file extension
-        :param file_name: str
-        :return: str
+        :param -> file_name: str
+        :return -> str
         """
+
         indx: int = file_name.rfind(".")
         return file_name[indx:] if indx != -1 else ""
 
@@ -193,9 +203,10 @@ class FileManager:
     def get_file_path_from_id(cls, file_id: str) -> str | FileNotFoundError:
         """
         Get file path from id
-        :param file_name: str
-        return: str | FileNotFoundError
+        :param -> file_id: str
+        :return -> str | FileNotFoundError
         """
+
         folder: str = cls.directory + "/" + file_id
 
         try:
@@ -215,10 +226,10 @@ class FileManager:
     def generate_file_path(cls, file_name: str, file_extension: str) -> str:
         """
         Generate file path
-        :param file_name: str
-        :param file_extension: str
-        :return: str
+        :param -> file_name: str, file_extension: str
+        :return -> str
         """
+
         os.makedirs(cls.directory + "/" + file_name, exist_ok=True)
         return cls.directory + "/" + file_name + "/" + cls.filename + file_extension
 
@@ -226,8 +237,8 @@ class FileManager:
     def get_transcription_files(cls, file_id: str) -> list[Path] | FileNotFoundError:
         """
         Get transcription files path
-        :param file_id: str
-        :return: list[Path] | FileNotFoundError
+        :param -> file_id: str
+        :return -> list[Path] | FileNotFoundError
         """
 
         folder: Path = Path(cls.directory + "/" + file_id)
@@ -251,27 +262,30 @@ class FileManager:
     def get_file_path(cls, file_id: str, file_extension: str) -> str:
         """
         Get file path
-        :param file_name: str
-        return: str
+        :param -> file_name: str, file_extension: str
+        :return -> str
         """
+
         return cls.directory + "/" + file_id + "/" + cls.filename + file_extension
 
     @classmethod
     def validate_file_path(cls, file_path) -> bool:
         """
         Validate file path
-        :param file_path: str
-        :return: bool
+        :param -> file_path: str
+        :return -> bool
         """
+
         return os.path.exists(file_path)
 
     @classmethod
     def delete_file(cls, file_path) -> None | FileNotFoundError:
         """
         Delete file
-        :param file_path: str
-        :return: bool | FileNotFoundError
+        :param -> file_path: str
+        :return -> bool | FileNotFoundError
         """
+
         if not cls.validate_file_path(file_path):
             raise FileNotFoundError
 
