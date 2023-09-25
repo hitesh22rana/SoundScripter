59d1057e944e8ee2b054171a0442ac75ee18127b
hitesh <hitesh22rana@gmail.com>
2023-09-25 13:34:31 +0530

    chore(feat): added open ports checks in check-requirements.sh

--- backend/check-requirements.sh
+++ backend/check-requirements.sh
@@ -1,22 +1,38 @@
-#!/bin/bash
+# !/bin/bash
 
 # Checks if Docker is installed
 if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Please install Docker and try again."
+    echo "Error: Docker is not installed. Please install Docker and try again."
     exit 1
 fi
 
 # Checks if Docker is running
 if ! docker info &> /dev/null; then
-    echo "Docker is installed but not running. Please start Docker and try again."
+    echo "Error: Docker is installed but not running. Please start Docker and try again."
     exit 1
 fi
 
 # Checks if Docker Compose is installed
 if ! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose is not installed. Please install Docker Compose and try again."
+    echo "Error: Docker Compose is not installed. Please install Docker Compose and try again."
     exit 1
 fi
 
-echo "Docker is installed and running."
+# Checks if port 8000, 5672 and 6379 are free and open
+for port in 8000 5672 6379; do
+    if [[ "$OSTYPE" == "msys" ]]; then
+        if netstat -an | findstr ":$port"; then
+            echo "Error: Port $port is already in use. Please free up port $port and try again."
+            exit 1
+        fi
+        
+    else
+        if nc -zv localhost $port 2>/dev/null; then
+            echo "Error: Port $port is already in use. Please free up port $port and try again."
+            exit 1
+        fi
+    fi
+done
+
+echo "Success: All requirements are met"
 exit 0
