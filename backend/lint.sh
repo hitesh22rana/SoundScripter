a34f1657fa50f445825e7e5d6041dac0a3d20808
hitesh <hitesh22rana@gmail.com>
2023-10-31 12:51:57 +0530

    chore: added lint and changed default format from black to ruff

--- backend/lint.sh
+++ backend/lint.sh
@@ -3,18 +3,15 @@
 # Activate the virtual environment
 source venv/Scripts/activate
 
-# Check if isort is already installed
-if ! pip3 show isort &>/dev/null; then
-    echo "isort is not installed. Installing now..."
-    pip3 install isort
-fi
-
 # Check if ruff is already installed
 if ! pip3 show ruff &>/dev/null; then
     echo "ruff is not installed. Installing now..."
     pip3 install ruff
 fi
 
-
-# Run isort and ruff to format the Python code
-isort . && ruff format .
+# Check if the --fix argument is provided
+if [[ "$1" == "--fix" ]]; then
+    ruff check . --fix
+else
+    ruff check .
+fi
