6fcbc82aa2f1d768725c29f14cbd8ad9b12d1260
hitesh <hitesh22rana@gmail.com>
2023-12-30 11:14:58 +0530

    feat: added loader component

---
+++ frontend/src/components/ui/loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="flex justify-center items-center w-full h-96">
+            <span className="loader" />
+        </div>
+    );
+};
+
+export default Loader;
