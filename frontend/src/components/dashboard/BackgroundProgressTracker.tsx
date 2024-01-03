6efe11e5a9ee9ac0574c39a6cfe007d245949d42
hitesh <hitesh22rana@gmail.com>
2024-01-03 09:49:10 +0530

    fix: key error handled

--- frontend/src/components/dashboard/BackgroundProgressTracker.tsx
+++ frontend/src/components/dashboard/BackgroundProgressTracker.tsx
@@ -4,20 +4,27 @@ import { Fragment } from "react";
 import useBackgroundProgressStore from "@/src/store/background-progress";
 
 const BackgroundProgressTracker = () => {
-    const { progressTracker } = useBackgroundProgressStore((state) => ({
-        progressTracker: state.progressTracker,
+    const { activeProgress } = useBackgroundProgressStore((state) => ({
+        activeProgress: state.activeProgress,
     }));
 
-    if (!progressTracker || progressTracker.length == 0) {
+    if (activeProgress.length == 0) {
         return null;
     }
 
+    const tasksInProgress: string =
+        activeProgress.length === 1
+            ? "1 task"
+            : `${activeProgress.length} tasks`;
+
     return (
         <div className="fixed left-1/2 right-1/2 -translate-x-1/2 bottom-0 max-w-lg w-full h-40 rounded-t-2xl bg-gray-100 border shadow flex flex-col gap-4 px-5 py-4">
-            <h3 className="font-medium text-lg">Uploading 1 item</h3>
+            <h3 className="font-medium text-lg">
+                {tasksInProgress} in progress
+            </h3>
 
             <div className="flex flex-col items-start gap-2">
-                {progressTracker.map((backgroundProgress, index) => {
+                {activeProgress.map((backgroundProgress, index) => {
                     return (
                         <Fragment key={index}>{backgroundProgress}</Fragment>
                     );
